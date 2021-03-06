439bcf5fac13a9cc026bb49c9e4d98e5daaa8a4c
Tomoyuki Tokuue <80094371+qtmm@users.noreply.github.com>
2021-03-06 17:25:19 +0900

    Modify setup.sh to consistent with README.md

--- setup.sh
+++ setup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # Disable beep sounds
-echo 'blacklist pcspkr' | sudo tee -a /etc/modprobe.d/blacklist.conf
+# echo 'blacklist pcspkr' | sudo tee -a /etc/modprobe.d/blacklist.conf
 
 # For AMD GPU #####
-# Change the backlight brightness
+## Change the backlight brightness
 echo 10 | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
+## Stop screen flickering
+echo '
+Section "Device"
+    Identifier "AMD"
+    Driver "amdgpu"
+    Option "TearFree" "true"
+EndSection
+' | sudo tee -a /etc/X11/xorg.conf.d/20-amdgpu.conf
 ###################
 
 # Adjust the clock
 sudo timedatectl set-ntp true
 
-# Update initial packages
-sudo pacman-mirrors --geoip && sudo pacman -Syu && paccache -ruk0
-
-# Install essential packages
-make essential
+# Change the default shell
+chsh -s $(which zsh)
 
 # Reboot
-# sudo shutdown -r now
-
-# Change the default shell
-# chsh -s $(which zsh)
+sudo shutdown -r now
